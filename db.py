3458824492a7d5fd08eeb52f81606cd2ac005186
Kamil Jakubisiak <u87_kamjak_waw@technischools.com>
2024-04-08 14:28:28 +0200

    hygw
    dwuygesluvuehfsciushdsyugd

--- db.py
+++ db.py
@@ -1,11 +1,22 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('postgresql://admin:admin@localhost:5432/po_db')
 
-Session = sessionmarker(bind=engine)
+Session = sessionmaker(bind=engine)
 session = Session()
 
-query = """
+with engine.connect() as con:
+    query = """
+    CREATE TABLE users (
+        id INTEGER,
+        name VARCHAR(100),
+    )
+    INSERT INTO users VALUES (1, 'Franciszek');
+    SELECT * FROM users;
+    """
 
+statement = text(query)
 
-"""
+result = con.execute(statement)
+print(result.first())
